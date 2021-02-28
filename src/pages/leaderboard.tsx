2f29ae3a1638d13c9ad3a4d4fe48a2f83536a059
Brian_Izaki <bkizaki@outlook.com>
2021-02-28 10:24:41 -0300

    feat: Leaderboard Layout

---
+++ src/pages/leaderboard.tsx
@@ -0,0 +1,70 @@
+import Head from 'next/head';
+import { MenuBar } from '../components/MenuBar';
+import { Profile } from '../components/Profile';
+import styles from '../styles/pages/LeaderBoard.module.css';
+
+interface UserCardTable {
+  position: number;
+  completedChallenges: number;
+  experience: number;
+}
+
+const UserTableCard: React.FC<UserCardTable> = ({
+  position,
+  completedChallenges,
+  experience,
+}: UserCardTable) => (
+  <tr>
+    <td>{position}</td>
+
+    <td>
+      <div>
+        <Profile />
+      </div>
+    </td>
+
+    <td>
+      <span>{completedChallenges}</span>
+      {' '}
+      feitos
+    </td>
+
+    <td>
+      <span>{experience}</span>
+      {' '}
+      xp
+    </td>
+  </tr>
+);
+
+export default function LeaderBoard(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>LeaderBoard | moveit</title>
+      </Head>
+
+      <MenuBar actualPage="leaderboard" />
+
+      <div className={styles.leaderBoardContainer}>
+        <h1>Leaderboard</h1>
+
+        <table className={styles.leaderBoardTable}>
+          <thead>
+            <tr>
+              <th>POSIÇÃO</th>
+              <th>USUÁRIO</th>
+              <th>DESAFIOS</th>
+              <th>EXPERIÊNCIA</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <UserTableCard position={1} completedChallenges={123} experience={12345} />
+            <UserTableCard position={2} completedChallenges={666} experience={55555} />
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
